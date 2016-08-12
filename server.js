da5cf8363dfc5fd669a670ad974bef9fe469d160
Ganesh Rajan <nr.ganesh281990@gmail.com>
2016-08-12 15:57:20 +0530

    services for adding service centers

--- server.js
+++ server.js
@@ -1,60 +1,99 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: "application/json" }));
+
 
 var fs = require("fs");
 
 
 app.all('/*', function(req, res, next) {
-  
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
 
 });
 
 
-app.get("/listUsers", function(req, res){
+app.post("/addServiceCenter", function(req, res) {
 
-	fs.readFile( __dirname + "/data/" + "users.json", 'utf8', function (err, data) {
-		console.log( data );
-		res.end( data );
-	});
+    var filename = __dirname + "/data/" + "servicecenters_entity.json";
+
+    fs.readFile(filename, 'utf8', function(err, data) {
+
+        var list = JSON.parse(data);
+
+        var newServiceCenter = {
+        	id: list.length + 1,
+            title: req.body.name,
+            address1: req.body.address1,
+            address2: req.body.address2,
+            address3: req.body.address3,            
+            contactNo: req.body.phoneNo,
+            websiteUrl: req.body.websiteUrl,
+            imageUrl: req.body.imageUrl,
+            searchPlaces: req.body.searchPlaces
+        };
+
+        list.push(newServiceCenter);
+
+        fs.writeFile(filename, JSON.stringify(list), function(err, data) {
+            if (err) {
+                return console.log(err);
+            }            
+        });
+
+        res.end("Success");
+    });
 
 });
 
 
-app.get("/getServiceCenters", function(req, res){
+app.get("/listUsers", function(req, res) {
 
-	fs.readFile( __dirname + "/data/" + "servicecenters.json", 'utf8', function (err, data) {
-		console.log( data );
-		res.end( data );
-	});
-
-});
-
-
-app.get("/getServiceCenterDetail", function(req, res){
-
-	fs.readFile( __dirname + "/data/" + "servicecenterdetail.json", 'utf8', function (err, data) {
-		console.log( data );
-		res.end( data );
-	});
+    fs.readFile(__dirname + "/data/" + "users.json", 'utf8', function(err, data) {
+        console.log(data);
+        res.end(data);
+    });
 
 });
 
 
+app.get("/getServiceCenters", function(req, res) {
+
+    fs.readFile(__dirname + "/data/" + "servicecenters.json", 'utf8', function(err, data) {
+        console.log(data);
+        res.end(data);
+    });
+
+});
+
+
+app.get("/getServiceCenterDetail", function(req, res) {
+
+    fs.readFile(__dirname + "/data/" + "servicecenterdetail.json", 'utf8', function(err, data) {
+        console.log(data);
+        res.end(data);
+    });
+
+});
 
 
 
-app.get('/', function (req, res) {
-   res.send('Hello World.js');
+
+
+app.get('/', function(req, res) {
+    res.send('Hello World.js');
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(8081, function() {
 
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log("Example app listening at http://%s:%s", host, port);
+    console.log("Example app listening at http://%s:%s", host, port);
 
 });
